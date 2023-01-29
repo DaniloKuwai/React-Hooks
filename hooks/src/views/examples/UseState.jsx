02f358c3dbdc099b0852167e72d5be34e3acb532
unknown <danilorkuwai@gmail.com>
2023-01-29 20:11:34 -0300

    aula 02 - useState 02

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -5,6 +5,7 @@ import SectionTitle from '../../components/layout/SectionTitle'
 const UseState = (props) => {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("Inicial")
 
     return (
         <div className="UseState">
@@ -24,6 +25,9 @@ const UseState = (props) => {
             </div>
 
             <SectionTitle title="Exercicio #02" />
+            <input type="text" className="input" 
+             value={name} onChange={e => setName(e.target.value)}  />
+             <span className="text">{name}</span>
         </div>
     )
 }
